8372134724dae1c35519faac5baa4326d2cd11b1
elinthornberg <piffiluren@hotmail.com>
2023-12-03 10:22:03 +0100

    Co-authored-by: Oliver-Timmerlid <Oliver-Timmerlid@users.noreply.github.com>

---
+++ main.js
@@ -0,0 +1,7 @@
+const checkBox = document.getElementById("divStyle");
+const textFields = document.querySelectorAll('.textfield');
+const button = document.querySelector('.button');
+const output = document.getElementById('output');
+
+console.log(checkBox);
+console.log(textFields);
