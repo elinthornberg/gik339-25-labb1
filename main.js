e8f5b75c31ad3499c1199dee0e04e07cbd7e8dc7
elinthornberg <piffiluren@hotmail.com>
2023-12-04 19:13:34 +0100

    Co-authored-by: Oliver-Timmerlid <Oliver-Timmerlid@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+//uppgift 4
 const checkBox = document.getElementById("divStyle");
 const textFields = document.querySelectorAll('.textfield');
 const button = document.querySelector('.button');
 const output = document.getElementById('output');
 
-console.log(checkBox);
-console.log(textFields);
+
+function handleEvent (e){
+   console.log('Den som genererade: ', e.target);
+   const temp = e.target.name;
+
+   if(temp === "content") {
+    const value = e.target.value;
+    output.innerHTML = '<p>' + value + '</p>';
+   }
+}
+
+
+checkBox.addEventListener('change', (e) => {
+    const color = textFields[0].value;
+    output.style.backgroundColor = color;
+});
+
+textFields.forEach((fields) => fields.addEventListener('blur', handleEvent));
+
+button.addEventListener('click', (e) => {
+    output.remove();
+});
+
